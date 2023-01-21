2a80f1321f0a080dd4566a60d5037cb28f605afa
Saymon Damásio <saymon.damasio95@gmail.com>
2023-01-21 19:13:19 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./styles/global.css";
-import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
 
 export function App() {
-  return <Habit />;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  );
 }
