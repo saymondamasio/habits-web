1b1df632163c154a381d5046215316bc90e14071
Saymon Damásio <saymon.damasio95@gmail.com>
2023-01-21 19:59:16 -0300

    feat: create summary table component

---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+type HabitDayProps = {
+  // completed: string;
+};
+
+export function HabitDay({}: HabitDayProps) {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  );
+}
